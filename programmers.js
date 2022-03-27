3e07816d41e9c17ff5956af500165e4a169b8466
Ain Chung <artnouveau0804@gmail.com>
2022-03-27 23:58:58 +0900

    javaScript shdowing & TimeTable
    파트10 더 알아보기 - 변수의 shadowing
    function name{
    //한 함수 안에서만 값이 유지되는 변수
    var in_fuction_var;
    }
    //여러 함수에서 값이 유지되면서 사용되는 변수
    var out_fuction_var;
    
    ex1)
    function shadowing_example(){
    	console.log(“f”, val);
    	val++;
    }
    var val = 0;
    shadowing_example();
    console.log(“e”,val);
    ==(f, 0)
    (e, 1)
    
    ex2)
    function shadowing_example(){
    	var val = 0;
    	console.log(“f”, val);
    	val++;
    }
    var val = 0;
    shadowing_example();
    console.log(“e”,val);
    ==(f, 0)
    (e, 0)
    
    파트10 더 알아보기 - 변수의 실습 shadowing
    구구단:
    function printTimesTable(a){
    	for(i = 1; i < 10; i++){
    		console.log(a + “*” + i + “=” + a*i);
    	}
    }
    for(a = 2; a < 10; a++){
    	printTimesTable(a);
    }
    1.함수 printTimesTable은 a라는 매개변수를 받는다.
    기능은: 반복문으로 i가 1부터 시작하고 i가 10보다 작은 9가 될때 까지 i를 업데이트.
    그리고 a곱하기 i는 ai라는걸 console.log하기(i는 구구단 오른쪽 숫자)
    
    2.그리고 a에 넣을 숫자를 만든다.
    반복문으로 a는 2부터 시작하게 하고 10보다 작은 9가 될때까지 a를 업데이트.
    
    3.그리고 반복문안에 함수를 넣어 실행시킨다.

--- programmers.js
+++ programmers.js
@@ -1,1 +1,9 @@
-console.log("hi");
+function printTimeTable(a) {
+  for (i = 1; i < 10; i++) {
+    console.log(a + "*" + i + "=" + a * i);
+  }
+}
+
+for (a = 1; a < 10; a++) {
+  printTimeTable(a);
+}
