3054e046d435e7b3af5c3ba449780f11bc82ede8
Ain Chung <artnouveau0804@gmail.com>
2022-03-30 20:15:31 +0900

    [class & 콜백 함수] (part.1)
    class Counter() {
    	constructor() {
    	  this.counter = 0;
    	}
    
    	increase() {
    	  this.counter++;
    	  console.log(this.counter);
    	}
    }
    
    const coolCounter = new Counter;
    coolCounter.increase;
    
    1. counter라는 class를 만들고 constructor(생성자)안에서 counter를 0으로 초기화 해준다.
    2. increase라는 함수를 만들어 이 함수가 실행되면 업데이트 되게 해준다. 업데이트 되면 console.log된다.
    3. coolCounter object에 새로운 Counter를 할당하고 업데이트 되면 counter는 여기에 저장되게 한다.
    4. counter가 할당된 coolcounter에 increase함수를 호출 한다.
    5. 호출 할 때마다 숫자가 업데이트 된다. //1 2 3
    
    ——[만약 5의 배수가 될 때마다 사용자에게 5의배수란걸 알려주고 싶다면]:
    1. if(this.counter % 5 === 0){
    	console.log(“5의배수”);
    }
    (this.counter 나누기 5의 몫이 0일때 console.log(“5의 배수”))
    
    ——[하지만 만약 “5의 배수”라는 문자를 바꾸고 싶거나, console.log말고 다른것도 실행하고 싶다면]:
    1. increase에 runIf5Times라는 인자를 주고, if{}에 runIf5Times()를 호출한다.(-> return이랑 비슷한 개념)
    2. new Counter아래에 function printSomething을 만들고 console.log(“5의 배수”)를 해준다.
    
    —— [이렇게 하면 5의 배수일 때마다 “5의 배수”가 나타나는데 출력 된 숫자도 옆에 나타나게 하고 싶다면]:
    1. if안에 있는 runIf5Times() parameter에 this.counter를 주고, function printSomething() parameter에는 num 변수를 준다.
    2. printSomething안에 있는 console.log()에도 ${num}을 주면 아래에서 coolCounter.increase(printSomething)을 실행하면서 num 자리에 this.counter가 나타난다.
    
    요약:
    1. increase(printSomething) 이 increase(runIf5Times)에 적용된다.
    
    /*
    function add(a, b) {
    	return a + b;
    }
    
    add(1, 2); //3
    */
    
    1과 2가 a와 b에 적용 돼서 서로 더해진 것처럼 printSomething이 runIf5Times에 적용된다.
    
    2. if문 안에 runIf5Times에 printSomething이 적용돼서 printSomething(this.counter)가 된다. this.counter는 5의 배수가 실행될 때마다 그 숫자가 출력 될 것.
    
    3. 그리고 바깥에 function printSomething(num) num자리에 this.counter가 온다.
    
    4. 그러면서 함수 안에 있는 console.log()안에 num에 5의 배수가 출력 됨.
    
    이해 못했던 부분:

--- ellie/ellie.js
+++ ellie/ellie.js
@@ -1,28 +1,31 @@
-class Person {
-  //constructor 생성자->나중에 object를 만들때 필요한 데이터를 전달함
-  constructor(name, age) {
-    //fields
-    //전달 된 데이터를 바로 할당해줌.
-    this.name = name;
-    this.age = age;
+class Counter {
+  constructor() {
+    this.counter = 0;
   }
-  //method
-  speak(message) {
-    console.log(`${this.name}: ${message}`);
+  increase(runIf5Times) {
+    this.counter++;
+    console.log(this.counter);
+    if (this.counter % 5 === 0) {
+      runIf5Times(this.counter);
+    }
   }
 }
-
-//new는 새로운 object를 만들때 쓰는 키워드
-const ellie = new Person("ellie", 20);
-console.log(ellie.name);
-console.log(ellie.age);
-//Person 안에는 speak이라는 function이 있기 때문에 new Person에서 만든 ellie라는 이름으로 speak를 실행 시킬 수 있음.
-ellie.speak("hello!");
-
-//new 뒤에는 class의 이름 Person이 와야한다.
-const zoe = new Person("zoe", 23);
-console.log(zoe.name);
-console.log(zoe.age);
-zoe.speak("hi");
-
-//두 person의 speak내용 다르게 하려면 매개변수 쓰기.
+const coolCounter = new Counter();
+function printSomething(num) {
+  console.log(`5의 배수 (${num})`);
+}
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
+coolCounter.increase(printSomething);
