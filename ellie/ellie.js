00e56951549dcc4f1984de6024f0a7f3e84d6f86
Ain Chung <artnouveau0804@gmail.com>
2022-04-01 23:49:12 +0900

    [getter & setter] (part.1)
    1. 우선 getter는 무언가를 획득, 받는 기능이고, setter는 설정하는 기능이다.
    2. 여기서 get은 minus함수의 결과를 get한다.

--- ellie/ellie.js
+++ ellie/ellie.js
@@ -1,55 +1,14 @@
-class Counter {
-  constructor(runEveryFiveTimes) {
-    this.counter = 0;
-    this.callback = runEveryFiveTimes;
-  }
-  increase() {
-    this.counter++;
-    console.log(this.counter);
-    if (this.counter % 5 === 0) {
-      this.callback(this.counter);
-    }
-  }
-}
-
-function printSomething(num) {
-  console.log(`5의 배수 (${num})`);
-}
-function alertNum(num) {
-  alert(`5의 배수 (${num})`);
-}
-
-const coolCounter = new Counter(printSomething);
-
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-coolCounter.increase();
-
-/*
-class minus {
+class calcAge {
   constructor(a, b) {
     this.a = a;
     this.b = b;
   }
   get age() {
-    return this.calcAge();
+    return this.minus();
   }
-  calcAge() {
+  minus() {
     return this.a - this.b;
   }
 }
-const sum = new minus(23, 2);
+const sum = new calcAge(23, 2);
 console.log(sum.age);
-*/
