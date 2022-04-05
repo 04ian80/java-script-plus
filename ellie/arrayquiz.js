0894945750db06c578490caf84e3ab69a6832b52
Ain Chung <artnouveau0804@gmail.com>
2022-04-05 23:37:24 +0900

    [ellie - array quiz](my solution)
    속성 중 하나를 사용하여 배열에서 객체 찾아내기.
    class Student {
      constructor(name, age, enrolled, score) {
        this.name = name;
        this.age = age;
        this.enrolled = enrolled;
        this.score = score;
      }
    }
    const students = [
      new Student('A', 29, true, 45),
      new Student('B', 28, false, 80),
      new Student('C', 30, true, 90),
      new Student('D', 40, false, 66),
      new Student('E', 18, true, 88),
    ];
    
    // Q5. 90점 이상인 학생 찾기
    
    그냥 함수:
    function findNinety(ninety) {
    	return ninety.score >= 90;
    }
    console.log(students.find(findNinety));
    
    화살표 함수 사용:
    const findNinety = students.find(ninety => ninety.score >= 90);
    console.log(findNinety)
    // Student {name: ‘C’, age: 30, enrolled: true, score: 90}
    
    해석:
    1. findNinety를 변수로 지정.
    2. (array name).find() 메소드를 이용해서 찾기 기능을 수행한다.
    3. ()괄호 안에는 student array안에서 찾을 수 있도록 매개변수를 준다.
    4. 그 다음 화살표 함수(=>)를 이용해 score의 값이 90보다 크거나 같을때 찾아내도록 한다.
    5. console.log를 하면 student의 정보가 나온다.
    arrow함수 문법: const varname = method(parameter => parameter.classParameter);
    
    Q6. enrolled가 true인 학생들의 정보를 추출. (.filter 메소드 사용)
    
    Q7. 객체배열의 속성중 score값들만 배열로 추출. (.map 메소드 사용)
    
    Q8. score이 50이하인 학생 추출. (.find 메소드 사용)
    
    Q9. 학생들 평균 점수 구하기 (평균구하는 함수를 만들고 for반복문 만들기. 그리고 만들어 놨던 findScore변수 사용하기.)
    
    Q10. 점수를 string으로 만들기. (.toString() 메소드 사용)
    
    Bonus. 점수 낮은 순부터 높은순으로 정렬하기. (sort 메소드 사용. return에 a - b) 그리고 .toString() 메소드 사용)

--- ellie/arrayquiz.js
+++ ellie/arrayquiz.js
@@ -1,37 +1,79 @@
 "use strict";
-//1
-const fruits = "apple, banana, orange";
-console.log(fruits);
+//1 make a string out of an array
+const fruits = ["apple", "banana", "orange"];
+const fruitsString = fruits.toString();
+console.log(fruitsString);
 
-//2
-const fruit = ["🍎, 🥝, 🍌, 🍒"];
-console.log(fruit);
+//2 make an array out of a string
+const fruit = "🍎, 🥝, 🍌, 🍒";
+const fruitArray = fruit.split(" "); //fruit.split("")를 할때 쌍따옴표 가운데에 스페이스를 줘야 한다.
+console.log(fruitArray);
 
-//3
+//3 make this array look like this: [5, 4, 3, 2, 1]
 const array = [1, 2, 3, 4, 5];
 array.reverse();
 console.log(array);
 
-//4
+//4 make new array without the first two elements
 const array2 = [1, 2, 3, 4, 5];
 array2.splice(0, 2);
 console.log(array2);
 
-//5
+//보기
 class Student {
-    constructor(name, age, enrolled, score) {
-      this.name = name;
-      this.age = age;
-      this.enrolled = enrolled;
-      this.score = score;
-    }
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
   }
-  const students = [
-    new Student('A', 29, true, 45),
-    new Student('B', 28, false, 80),
-    new Student('C', 30, true, 90),
-    new Student('D', 40, false, 66),
-    new Student('E', 18, true, 88),
-  ];
+}
+const students = [
+  new Student("A", 29, true, 45),
+  new Student("B", 28, false, 80),
+  new Student("C", 30, true, 90),
+  new Student("D", 40, false, 66),
+  new Student("E", 18, true, 88),
+];
 
- const goodStudent = 
+array;
+// 5 find a student with the score 90
+const findNinety = students.filter((ninety) => ninety.score >= 90);
+console.log(findNinety); // find 메소드는 90점이상 요소 중 첫번째만 출력
+
+// 6 make an array of enrolled students
+const result = students.filter((value) => value.enrolled === true);
+console.log(result);
+
+// 7 make an array containing only the students' scores
+// result should be: [45, 80, 90, 66, 88]
+const findScore = students.map((find) => find.score);
+console.log(findScore); //객체 배열에서 속성 값을 배열로 추출
+
+// 8 check if there is a student with the score lower than 50
+const ifLowerThan50 = students.find(function (item) {
+  return item.score < 50;
+});
+console.log(ifLowerThan50); //배열에서 특정 객체 찾기.
+
+// 9 compute students' average score
+function average(findScore) {
+  let sum = 0;
+  for (let i = 0; i < findScore.length; i++) {
+    sum += findScore[i];
+    return sum / findScore.length;
+  }
+}
+console.log(average(findScore));
+
+// 10 make a string containing all the scores
+// result should be: '45, 80, 90, 66, 88'
+const stringScore = findScore.toString();
+console.log(stringScore);
+
+// Bonus! do Q10 sorted in ascending order
+// result should be: '45, 66, 80, 88, 90'
+findScore.sort(function (a, b) {
+  return a - b;
+});
+console.log(findScore.toString());
