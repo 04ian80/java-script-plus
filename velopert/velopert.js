1fdf0f09f2e0a81b0261026770ff59302aa648da
Ain Chung <artnouveau0804@gmail.com>
2022-05-26 03:01:23 +0900

    [velopert] - 객체/객체비구조화할당/this
    1. 객체 key와 value 꺼내오기
    2. 객체 비구조화 할당: {}안에 객체의 값들을 추출해 새로운 상수로선언. 파라미터에 넣을 수도 있음.
    3. this: 값이 속한 객체 이름을 this로 대체 할 수 있음. 하지만 화살표 함수는 안됨.

---
+++ velopert/velopert.js
@@ -0,0 +1,78 @@
+//함수
+const bride = {
+  name: 'monica',
+  gender: 'women',
+  age: 26,
+  job: 'chef',
+};
+
+const groom = {
+  name: 'chandler',
+  gender: 'men',
+  age: 28,
+  job: 'transponter',
+};
+
+function introduce(person) {
+  if (person.gender === 'women') {
+    person.gender = 'she';
+  } else {
+    person.gender = 'he';
+  }
+  const say = `${person.name} is ${person.age}years-old and ${person.gender} is ${person.job}`;
+  console.log(say);
+}
+introduce(bride); //monica is 26years-old and she is chef
+introduce(groom); //chandler is 28years-old and he is transponter
+
+//객체 비구조화 할당
+function introduce(person) {
+  const { name, age, job } = person;
+  let { gender } = person;
+  if (gender === 'women') {
+    gender = 'she';
+  } else {
+    gender = 'he';
+  }
+  const say = `${name} is ${age}years-old and ${gender} is ${job}`;
+  console.log(say);
+}
+introduce(bride); //위와 동일
+introduce(groom); //위와 동일
+
+//gender은 어디에 넣는 거지
+function introduce({ gender, name, age, job }) {
+  if (gender === 'women') {
+    gender = 'she';
+  } else {
+    gender = 'he';
+  }
+  const say = `${name} is ${age}years-old and ${gender} is ${job}`;
+  console.log(say);
+}
+introduce(bride); //위와 동일
+introduce(groom); //위와 동일
+
+//this
+const dog = {
+  name: 'pet',
+  sound: 'bow-wow',
+  bark: function () {
+    console.log(this.sound);
+  },
+};
+
+dog.bark(); //bow-wow
+//이 함수에서 this는 자신이 속한 객체를 가리키는데, 화살표 함수는 그렇지 않음.
+
+const anotherDog = {
+  name: 'pet',
+  sound: 'bow-wow',
+  bark: () => {
+    console.log(this.sound);
+  },
+};
+
+anotherDog.bark(); //undefined
+
+//
